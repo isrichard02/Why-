b462a03606081b35c6c1547f4e2a92e84e0b1e2c
isrichard02 <rlheilbron@mymail.mines.edu>
2019-11-21 14:51:25 -0700

    The 2nd function

--- Week12-utility.py
+++ Week12-utility.py
@@ -7,3 +7,15 @@ def PrintOutput(f):
     result = f
     print('OUTPUT',result)
 
+import string
+def LoadFile(x):
+    with open(x, 'r') as f:
+        ending = f.readlines()
+        re = []
+        for i in ending:
+            temp = ''
+            for j in i:
+                if j in string.ascii_letters or j == ' ':
+                    temp +=j
+            re.append(temp)            
+    return(re)
